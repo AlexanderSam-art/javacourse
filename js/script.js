866d6a50c68ffd8d58d9a78ee97cc2f8eaeefdea
Alexander <alexsamfree@gmail.com>
2020-12-08 13:38:07 +0300

    new files1

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//alert('Hello');
+
+//const result =  confirm("Are you here?");
+//console.log(result);
+//const answer = prompt("Вам есть 18?", "18");
+//console.log(typeof(answer));
+
+const answers = [];
+
+//answers[0] = prompt('Your name?','');
+//answers[1] = prompt('Your last name?','');
+//answers[2] = prompt('How old you?','');
+
+console.log(typeof(answers));
+
---
+++ js/script9.js
@@ -0,0 +1,21 @@
+"use strict";
+
+console.log('arr' + " - object");
+console.log(4 + "5");
+
+let incr = 10,
+    decr = 10;
+
+    //incr++;//увелечение значения на 1
+    //decr--;//уменьшение значения
+    console.log(++incr);
+    console.log(--decr);
+
+    console.log(5%2);
+
+    console.log(2 + 2 * 2 != 8); //=== Строгое равенство
+
+    const isChecked = false,
+          isClose = false;
+
+          console.log(isChecked || !isClose);
